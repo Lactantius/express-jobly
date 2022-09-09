6e0bfaf89f606a4ec346d65a83ed313a197c665e
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-09 14:54:23 -0500

    Add test for correct error in sqlForPartialUpdate

--- __tests__/helpers/sql.test.ts
+++ __tests__/helpers/sql.test.ts
@@ -1,3 +1,4 @@
+import { BadRequestError } from "../../src/expressError";
 import { sqlForPartialUpdate } from "../../src/helpers/sql";
 
 describe("sqlForPartialUpdate", () => {
@@ -12,5 +13,13 @@ describe("sqlForPartialUpdate", () => {
       setCols: '"snake_case"=$1, "more_snake_case"=$2',
       values: ["text", 50],
     });
+  });
+  test("throws error if no data entered", () => {
+    const data = {};
+    const mapping = {
+      camelCase: "snake_case",
+      moreCamelCase: "more_snake_case",
+    };
+    expect(() => sqlForPartialUpdate(data, mapping)).toThrow(BadRequestError);
   });
 });
